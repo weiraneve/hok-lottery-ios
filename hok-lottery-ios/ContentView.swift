358e5ca452254ea13c4e468cef94f2a6649e4bfa
weiran1999 <907221539@qq.com>
2023-02-19 23:31:11 +0800

    Initial Commit

---
+++ hok-lottery-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hok-lottery-ios
+//
+//  Created by Weiran Sun on 2023/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hok-lottery-ios/hok_lottery_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hok_lottery_iosApp.swift
+//  hok-lottery-ios
+//
+//  Created by Weiran Sun on 2023/2/19.
+//
+
+import SwiftUI
+
+@main
+struct hok_lottery_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
